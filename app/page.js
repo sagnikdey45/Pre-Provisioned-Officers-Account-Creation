17e6302e3af7040596b3a87c7e2998a7ba1caa40
sagnikdey45 <deydsagnik@gmail.com>
2026-03-24 07:04:47 +0530

    Updated the root route for redirection to system admin page.

--- app/page.js
+++ app/page.js
@@ -3,16 +3,17 @@
 import React from "react";
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
   return (
     <div className="bg-white dark:bg-black flex justify-center items-center h-screen flex-col gap-4">
       <h1 className="text-3xl font-bold text-green-800 dark:text-teal-300">
         Home Page
       </h1>
-      <Button variant="outline" onClick={() => toast.success("Hello, World!")}>
-        Click Me
+      <Button variant="outline" onClick={() => router.push("/system-admin")}>
+        Go to System Admin Page
       </Button>
       <ModeToggle />
     </div>
